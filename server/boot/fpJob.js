642f86449e32c5dadf850bcee91320555dc45e6e
Luis Palomino Trevilla <luispalominot3@gmail.com>
2018-12-23 22:28:39 -0600

    FP formula added

--- server/boot/fpJob.js
+++ server/boot/fpJob.js
@@ -33,30 +33,47 @@ const http = require('http');
 moment.tz.setDefault("America/Mexico_City");
 var timezone = 'America/Mexico_City';
 
+const fpFormula = function(P, Q) {
+    return P/Math.sqrt(Math.pow(P, 2) + Math.pow(Q, 2));
+}
+
 var fpReadings = new CronJob('*/30 * * * *', function () {
     Meters.getActivesAssigned(function(err, meters) {
         async.each(meters, function(meter, next){
             var dates = EDS.dateFilterSetup(Constants.Meters.filters.monthAVG);
-            let serviceToCall = meter.hostname+ API_PREFIX +"records.xml" + "?begin=" +dates.begin+ "?end="
-                +dates.end + "?var=" +meter.summatory_device+ "." +Constants.Meters.common_names.summatory_pf + "?period=" + dates.period;
+            let serviceToCall = meter.hostname+ API_PREFIX +"records.xml" + "?begin=" +dates.begin+ "?end=" +dates.end 
+            meter.devices.map(device => {
+                serviceToCall += "?var="+device+".EPimp"+"?var="+device+".EQimp";
+            });
+            serviceToCall += "?period=" + dates.period;
 
             // console.log('service to call:', serviceToCall);
             xhr.open('GET', serviceToCall, false);
             xhr.onreadystatechange = function(){
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    var reading = Converter.xml2js(xhr.responseText, OPTIONS_XML2JS);
-                    let summatory = 0;
+                    const reading = Converter.xml2js(xhr.responseText, OPTIONS_XML2JS);
+                    let P = 0;
+                    let Q = 0;
                     if(reading.recordGroup.record){
-                        summatory = parseFloat(reading.recordGroup.record.field.value._text) * 100;
-                        summatory = summatory.toFixed(2);
-                        // console.log('FP: '+ meter.device_name + ': value => ' + summatory);
+                        reading.recordGroup.record.field.map(read => {
+                            const type = read.id._text.split('.')[1];
+                            let value = parseFloat(read.value._text);
+                            if (type === 'EPimp') {
+                                P += value;
+                            } else {
+                                Q += value;
+                            }
+                        });
+                        const fp = (fpFormula(P, Q)*100).toFixed(2);
                         meter.latestValues.lastUpdated = new Date();
                         if(!meter.latestValues.fp){
                             meter.latestValues.fp = {};
-                            meter.latestValues.fp.value = summatory;
-                        } else {
-                            meter.latestValues.fp.value = summatory;
                         }
+                        meter.latestValues.fp.value = fp;
+                        if (!meter.latestValues.reactive){
+                            meter.latestValues.reactive = {}
+                        }
+                        meter.latestValues.reactive.value = Q.toFixed(2);
 
                         let company_id = meter.company().id;
                         meter.unsetAttribute("company");
@@ -65,8 +82,14 @@ var fpReadings = new CronJob('*/30 * * * *', function () {
                             if(err) next(err, null);
                             else {
                                 let socketData = {
-                                    socketEvent: 'odometerReading',
-                                    data: meter.latestValues.dp
+                                    socketEvent: 'powerFactor',
+                                    data: meter.latestValues.fp
+                                };
+                                socketData = JSON.stringify(socketData);
+                                Socket.sendMessageToCompanyUsers(company_id, socketData);
+                                socketData = {
+                                    socketEvent: 'reactive',
+                                    data: meter.latestValues.reactive
                                 };
                                 socketData = JSON.stringify(socketData);
                                 Socket.sendMessageToCompanyUsers(company_id, socketData);
