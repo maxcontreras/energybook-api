63bb943949b12f45e6a08724f12c5acbfbbb5414
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-03-09 00:01:51 -0600

    Change Job run times

--- server/boot/fpJob.js
+++ server/boot/fpJob.js
@@ -4,8 +4,10 @@ const DesignatedMeter = app.loopback.getModel('DesignatedMeter');
 
 const timezone = 'America/Mexico_City';
 
-var fpReadings = new CronJob('*/30 * * * *', function () {
+var fpReadings = new CronJob('5,35 * * * *', function () {
+    console.log('Starting fp readings job')
     DesignatedMeter.fpReadings(undefined, (err, res) => {
+        console.log('Finished fp readings job')
         if (err) console.log("Error ", JSON.stringify(err, null, 2));
     });
 }, function () {
